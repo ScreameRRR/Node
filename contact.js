12a2659212568460fd6f3c57a8f8c198eff5ae77
ScreameRRR <68212647+ScreameRRR@users.noreply.github.com>
2022-07-10 21:34:45 +0300

    getContact function was added and tested

---
+++ contact.js
@@ -0,0 +1,28 @@
+const fs = require('fs').promises;
+const path = require('path');
+const contactsPath = path.join(__dirname, "/db/contacts.json")
+ 
+const listContacts = async () => {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts;
+}
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
+}
---
+++ index.js
@@ -0,0 +1,29 @@
+const contactListOperations = require("./contact.js");
+const argv = require("yargs").argv;
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+        const allContacts = await contactListOperations.listContacts();
+        console.log(allContacts)
+
+          break;
+
+    // case "get":
+    //   // ... id
+    //   break;
+
+    // case "add":
+    //   // ... name email phone
+    //   break;
+
+    // case "remove":
+    //   // ... id
+    //   break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+invokeAction(argv);
+
